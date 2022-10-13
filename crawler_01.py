2672352082a8f6ebdfbdc4aaf2302cb2c873a553
seokjun jin <bbjoony@gmail.com>
2022-10-13 21:36:27 +0900

    명언 1차 크롤링

--- crawler_01.py
+++ crawler_01.py
@@ -1,2 +1,15 @@
 import os, re, usecsv
 import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup as bs
+
+url = 'http://quotes.toscrape.com/'
+html = ur.urlopen(url)
+
+soup = bs(html.read(), 'html.parser')
+
+pretext = soup.find_all('div', {"class" : "quote"}) #배열 형태로 반환
+# print(pretext[0].text)
+
+for i in pretext:
+    print(i.text)
